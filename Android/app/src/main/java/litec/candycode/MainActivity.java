c4a95c593fd52c7a0d82b9f64d9426102d0214ed
KAINANIK <40146720140048@litec.ac.at>
2018-03-15 09:01:37 +0100

    Android CandyCode created

--- Android/app/src/main/java/litec/candycode/MainActivity.java
+++ Android/app/src/main/java/litec/candycode/MainActivity.java
@@ -2,6 +2,7 @@ package litec.candycode;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //R -> gradle erzwugt resource klasse -> zugriff auf xml datein/elemente
+
+        TextView textview = (TextView) this.findViewById(R.id.textView_Products);
+        textview.setText("Products");
     }
 }
